b16e10f6e4e36005c52bfb7044651716d26c4c4a
kongdori <nckres14@gmail.com>
2021-12-23 19:53:32 +0900

    Add google fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,24 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-export default class MyDocument extends Document {
+class MyDocument extends Document {
     render() {
         return (
             <Html lang="ko">
-                <Head />
+                <Head>
+                    <link
+                        rel="preconnect"
+                        href="https://fonts.googleapis.com"
+                    />
+                    <link
+                        rel="preconnect"
+                        href="https://fonts.gstatic.com"
+                        crossOrigin=""
+                    />
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&family=Noto+Sans+KR:wght@400;500;700&display=swap"
+                        rel="stylesheet"
+                    />
+                </Head>
                 <body>
                     <Main />
                     <NextScript />
@@ -13,3 +27,5 @@ export default class MyDocument extends Document {
         );
     }
 }
+
+export default MyDocument;
