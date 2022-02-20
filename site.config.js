6a7a9eb37a54506d5a5446ec20bce96f82fe6ef2
kongdori <nckres14@gmail.com>
2022-02-20 16:26:23 +0900

    Update Site Config - globalNav 상단 전역 메뉴 - confNav 상단 오른쪽 설정 메뉴

--- site.config.js
+++ site.config.js
@@ -1,48 +1,42 @@
 module.exports = {
     siteName: '나노디비',
     header: {
-        topNav: {
+        globalNav: [
+            {
+                name: '게임',
+                href: '/games',
+                subNav: [
+                    {
+                        name: '한국어화',
+                        href: '/game/hangeuls',
+                        io5Icon: 'IoLanguage'
+                    }
+                ]
+            }
+        ],
+        confNav: {
             links: [
                 {
-                    key: 'github',
                     href: 'https://github.com/nanodbnet',
-                    label: '나노디비 깃허브',
+                    name: 'Github',
                     faIcon: 'FaGithub'
                 },
                 {
-                    key: 'steam',
                     href: 'https://steamcommunity.com/groups/nanodbnet',
-                    label: '나노디비 스팀',
+                    name: 'Steam',
                     faIcon: 'FaSteam'
                 }
             ],
-            mail: {
-                label: '나노디비 문의',
-                faIcon: 'FaEnvelope',
-                mails: [
-                    {
-                        title: '나노디비 문의',
-                        address: 'nano@nanodb.net'
-                    },
-                    {
-                        title: '개인 문의',
-                        address: 'nckres14@gmail.com'
-                    }
-                ]
-            }
-        },
-        globalNav: [
-            {
-                key: 'games',
-                name: '게임',
-                href: '/games/',
-                current_match: false,
-                subNav: [
-                    {
-                        name: '한국어화'
-                    }
-                ]
-            }
-        ]
+            mails: [
+                {
+                    title: '나노디비 문의',
+                    address: 'nano@nanodb.net'
+                },
+                {
+                    title: '개인 문의',
+                    address: 'nckres14@gmail.com'
+                }
+            ]
+        }
     }
 };
