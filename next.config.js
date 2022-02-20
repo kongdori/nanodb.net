5196d5134de309570cb8009f83bfa334edf7a675
kongdori <nckres14@gmail.com>
2022-02-20 16:22:55 +0900

    Update Next.js Config - trailingSlash URL slash - images.domains 스팀 cdn 등록 - experimental.scrollRestoration 뒤로가기 스크롤 유지 (실험 버전) - redirects / to /games/

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
-/** @type {import('next').NextConfig} */
 module.exports = {
-    reactStrictMode: true
+    reactStrictMode: true,
+    trailingSlash: true,
+    images: {
+        domains: ['cdn.cloudflare.steamstatic.com']
+    },
+    experimental: {
+        scrollRestoration: true
+    },
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: '/games/',
+                permanent: true
+            }
+        ];
+    }
 };
