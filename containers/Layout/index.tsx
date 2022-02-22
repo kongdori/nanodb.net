044bb8b4f9a335210983b4e5ba9bda1f6b70bfa2
kongdori <nckres14@gmail.com>
2022-02-22 20:17:21 +0900

    Move 시스템 테마별 파비콘 및 기타 메타 태그 레이아웃에서 처리

--- containers/Layout/index.tsx
+++ containers/Layout/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import Head from 'next/head';
 import Link from 'next/link';
 import Logo from '@components/Logo';
+import { useTheme } from 'next-themes';
+import { siteName } from 'site.config';
 import { GlobalNav, ConfNav } from './Nav';
 
 interface LayoutContainerProps {
@@ -8,8 +11,28 @@ interface LayoutContainerProps {
 }
 
 export default function LayoutContainer({ children }: LayoutContainerProps) {
+    const { systemTheme } = useTheme();
+    const favicon = systemTheme === 'dark' ? 'favicon-dark' : 'favicon';
+
     return (
         <>
+            <Head>
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1, shrink-to-fit=no"
+                />
+                <link
+                    rel="icon"
+                    type="image/svg+xml"
+                    href={`/favicons/${favicon}.svg`}
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    href={`/favicons/${favicon}.png`}
+                />
+                <title>{siteName}</title>
+            </Head>
             <header className="w-full sticky top-0 z-40 h-12">
                 <div className="relative px-4 h-full">
                     <div className="absolute w-full h-full left-0 right-0 top-0 bottom-0 backdrop-blur border-b shadow-sm border-b-black/10 bg-white/90 dark:border-b-white/10 dark:bg-dark/90" />
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,39 +1,17 @@
 import { AppProps } from 'next/app';
-import Head from 'next/head';
 import { ThemeProvider } from 'next-themes';
 import NextNprogress from 'nextjs-progressbar';
-import { siteName } from 'site.config.js';
 import Layout from '@containers/Layout';
 
 import '@styles/common.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-    <>
-        <Head>
-            <meta
-                name="viewport"
-                content="width=device-width, initial-scale=1, shrink-to-fit=no"
-            />
-            <link
-                rel="icon"
-                type="image/svg+xml"
-                href="/favicons/favicon.svg"
-            />
-            <link
-                rel="icon"
-                type="image/png"
-                sizes="32x32"
-                href="/favicons/favicon.png"
-            />
-            <title>{siteName}</title>
-        </Head>
-        <ThemeProvider attribute="class" defaultTheme="system">
-            <NextNprogress color="#3b82f6" />
-            <Layout>
-                <Component {...pageProps} />
-            </Layout>
-        </ThemeProvider>
-    </>
+    <ThemeProvider attribute="class" defaultTheme="system">
+        <NextNprogress color="#3b82f6" />
+        <Layout>
+            <Component {...pageProps} />
+        </Layout>
+    </ThemeProvider>
 );
 
 export default MyApp;
