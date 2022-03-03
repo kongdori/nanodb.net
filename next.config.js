59c53fbceb9ac2238ec7520c1ac7ea9ef701e59c
kongdori <nckres14@gmail.com>
2022-03-03 14:48:06 +0900

    Add 스팀 CDN akamai

--- next.config.js
+++ next.config.js
@@ -2,7 +2,10 @@ module.exports = {
     reactStrictMode: true,
     trailingSlash: true,
     images: {
-        domains: ['cdn.cloudflare.steamstatic.com']
+        domains: [
+            'cdn.cloudflare.steamstatic.com',
+            'cdn.akamai.steamstatic.com'
+        ]
     },
     experimental: {
         scrollRestoration: true
