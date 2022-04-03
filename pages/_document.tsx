675fa16332162c55e71bdc9276a68fae70a9d858
kongdori <nckres14@gmail.com>
2022-04-04 02:04:48 +0900

    update fonts link loader

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,7 +15,7 @@ class MyDocument extends Document {
                         crossOrigin=""
                     />
                     <link
-                        href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&family=Noto+Sans+KR:wght@400;500;700&display=swap"
+                        href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Noto+Sans+KR:wght@400;500;700&display=swap"
                         rel="stylesheet"
                     />
                 </Head>
